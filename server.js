34cec2776ec0f13466c8df0add91c8b75774bf69
Alexander De Michieli <ademichieli@squarespace.com>
2021-06-03 14:30:18 -0700

    pushing to heroku

--- server.js
+++ server.js
@@ -20,16 +20,16 @@ app.use('/api/profile', require ('./routes/api/profile'))
 app.use('/api/posts', require ('./routes/api/posts'))
 app.use('/api/auth', require ('./routes/api/auth'))
 
-// Serve static asssets in prod
+// // Serve static asssets in prod
 
-if (process.env.NODE_ENV === 'production'){
-    //set statuc folder
-    app.use(express.static('client/build'))
-    app.get('*', (req, res)=>{
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
+// if (process.env.NODE_ENV === 'production'){
+//     //set statuc folder
+//     app.use(express.static('client/build'))
+//     app.get('*', (req, res)=>{
+//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+//     })
 
-}
+// }
 
 //Setting port
 const PORT = process.env.PORT || 5000
