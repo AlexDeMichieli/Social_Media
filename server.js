51421b6f980569fe1018c3d3b16099213e25bd2c
Alexander De Michieli <ademichieli@squarespace.com>
2021-06-03 13:50:53 -0700

    prepared for deployment

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express')
 const router = express.Router()
 const connectDB = require ('./config/db')
-
+const path = require('path')
 const app = express()
 
 //connect DB
@@ -14,7 +14,6 @@ app.use(express.json({extended: false}))
 //app.use(bodyParser.json())
 
 
-app.get('/', (req, res)=> res.send('API RUNNING'))
 
 //Setting port
 const PORT = process.env.PORT || 5000
@@ -25,3 +24,14 @@ app.use('/api/users', require ('./routes/api/users'))
 app.use('/api/profile', require ('./routes/api/profile'))
 app.use('/api/posts', require ('./routes/api/posts'))
 app.use('/api/auth', require ('./routes/api/auth'))
+
+// Serve static asssets in prod
+
+if (process.env.NODE_ENV === 'production'){
+    //set statuc folder
+    app.use(express.static('client/build'))
+    app.get('*', (req, res)=>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+
+}
